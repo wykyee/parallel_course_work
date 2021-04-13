81f186b9fa87db984a657ceecfbd9ddccf8b60e5
wykyee <danil.vasiliuk@gmail.com>
2021-04-13 15:11:57 +0300

    feat: get data from .json file and allow clients use it

--- src/client_server/Server.java
+++ src/client_server/Server.java
@@ -1,30 +1,50 @@
 package client_server;
 
-import java.net.*;
-import java.io.*;
+import com.google.gson.Gson;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class Server {
     public static final int PORT = 5001;
+    private static final String SAVE_FILE_PATH = "C:\\Users\\wykyee\\IdeaProjects\\course\\sample.json";
 
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(PORT);
         System.out.println("Server Started");
 
+        Gson gson = new Gson();
+        HashMap<String, ArrayList<String>> map = gson.fromJson(new FileReader(SAVE_FILE_PATH), HashMap.class);
+
         while (true) {
             Socket socket = null;
+            DataInputStream in = null;
+            DataOutputStream out = null;
             try {
                 socket = server.accept();
                 System.out.println("New client connected");
 
-                DataInputStream in = new DataInputStream(socket.getInputStream());
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-                Thread clientManager = new ClientManager(socket, in, out);
+                in = new DataInputStream(socket.getInputStream());
+                out = new DataOutputStream(socket.getOutputStream());
+                Thread clientManager = new ClientManager(socket, in, out, map);
                 clientManager.start();
-
             } catch (IOException e) {
-                socket.close();
+                if (socket != null) {
+                    socket.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
             }
         }
     }
@@ -34,17 +54,21 @@ class ClientManager extends Thread {
     final Socket socket;
     final DataInputStream in;
     final DataOutputStream out;
+    final HashMap<String, ArrayList<String>> map;
     private static final String EXIT_WORD = "-quit";
 
-    public ClientManager(Socket socket, DataInputStream in, DataOutputStream out) {
+    public ClientManager(Socket socket, DataInputStream in,
+                         DataOutputStream out, HashMap<String, ArrayList<String>> map) {
         this.socket = socket;
         this.in = in;
         this.out = out;
+        this.map = map;
     }
 
     @Override
     public void run() {
         String userInput;
+        ArrayList<String> result;
         while (true) {
             try {
                 out.writeUTF("Enter word to find or '-quit' to exit");
@@ -54,8 +78,13 @@ class ClientManager extends Thread {
                     out.writeUTF("Bye bye");
                     break;
                 }
-                System.out.println("USER WROTE: " + userInput);
-                out.writeUTF("Server response test");
+                System.out.println("USER IS LOOKING FOR: " + userInput);
+                result = map.get(userInput);
+                if (result != null) {
+                    out.writeUTF(result.toString());
+                } else {
+                    out.writeUTF("Files don't contain word '" + userInput + "'");
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
